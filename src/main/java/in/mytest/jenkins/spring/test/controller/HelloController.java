c8c496901bb099ee90f5ccfadae9c45841a8a932
venkat <injarapu.venkat@gmail.com>
2022-04-29 16:02:51 +0530

    3rd commit

--- src/main/java/in/mytest/jenkins/spring/test/controller/HelloController.java
+++ src/main/java/in/mytest/jenkins/spring/test/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     private String instance = "Test-Machine";
-    private String name = "SPRINGHERO";
+    private String name = "venkat";
 
     @Autowired
     private HelloService hs;
